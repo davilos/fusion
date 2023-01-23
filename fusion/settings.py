ecff718aa9b2e6d3cf0a58cdf44571c5bd0ff1d9
Dávilos Tavares <daviloscostagg@hotmail.com>
2023-01-23 02:16:30 -0300

    removes storage

--- fusion/settings.py
+++ fusion/settings.py
@@ -136,7 +136,6 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_ROOT = BASE_DIR / 'media'
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
