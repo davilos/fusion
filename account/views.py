d5ab8bc7865c559ccf637c8cef42eba065842f16
Dávilos Tavares <daviloscostagg@hotmail.com>
2023-03-07 17:18:43 -0300

    adds type hints to the account's apps.py and forms.py files

--- account/views.py
+++ account/views.py
@@ -1,14 +1,19 @@
+from typing import Any
+
 from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm
+from django.http import HttpResponse
 from django.urls import reverse_lazy
-from django.views import generic
+from django.views.generic import CreateView
 
 
-class Register(generic.CreateView):
-    form_class = UserCreationForm
-    success_url = reverse_lazy('login')
+class Register(CreateView):
+    form_class: type[UserCreationForm] = UserCreationForm
+    success_url: str | Any | None = reverse_lazy('login')
     template_name: str = 'cadastro.html'
 
-    def form_valid(self, form, *args, **kwargs):
+    def form_valid(
+        self, form: UserCreationForm, *args, **kwargs
+    ) -> HttpResponse:
         messages.success(self.request, 'Cadastro realizado com sucesso!')
         return super(Register, self).form_valid(form, *args, **kwargs)
