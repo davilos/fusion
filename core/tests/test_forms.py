b7b12df5b25d4748272f10d98fc2a71be41959e0
Dávilos Tavares <daviloscostagg@hotmail.com>
2023-03-07 17:14:50 -0300

    adds tests to the core forms.py

---
+++ core/tests/test_forms.py
@@ -0,0 +1,33 @@
+from django.test import TestCase
+
+from core.forms import ContatoForm
+
+CONTATO = ContatoForm
+
+
+class ContatoFormTestCase(TestCase):
+    def setUp(self) -> None:
+        self.nome: str = 'Felicity Jones'
+        self.email: str = 'felicity@gmail.com'
+        self.assunto: str = 'Um assunto qualquer'
+        self.mensagem: str = 'Uma mensagem qualquer'
+
+        self.dados: dict[str, str] = {
+            'nome': self.nome,
+            'email': self.email,
+            'assunto': self.assunto,
+            'mensagem': self.mensagem,
+        }
+
+        self.form: CONTATO = ContatoForm(
+            data=self.dados
+        )  # ContatoForm(request.POST)
+
+    def test_send_mail(self) -> None:
+        form1: CONTATO = ContatoForm(data=self.dados)
+        form1.is_valid()
+
+        form2: CONTATO = self.form
+        form2.is_valid()
+
+        self.assertEquals(form1.is_valid(), form2.is_valid())
